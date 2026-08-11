5e865184cc4db46ba091819974abed08f5725fbf
Prg-Yash <127442680+Prg-Yash@users.noreply.github.com>
2026-08-12 01:30:34 +0530

    feat: add root layout with font configuration, site metadata, and custom favicon assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,10 @@ export default function RootLayout({
       className={`${playfair.variable} ${ibmPlexMono.variable} ${cormorant.variable} ${jetbrains.variable} ${inter.variable} h-full antialiased bg-[#1d1d1d] text-[#f5f0e8]`}
     >
       <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=3" />
+        <link rel="icon" type="image/png" href="/icon.png?v=3" />
+        <link rel="shortcut icon" href="/favicon.ico?v=3" />
+        <link rel="apple-touch-icon" href="/icon.png?v=3" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
